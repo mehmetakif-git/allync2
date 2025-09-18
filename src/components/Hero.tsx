c96e11ce3dde7ad380d7d5fc59a9d4660915a5d1
mehmetakif-git <mehmetakifaybey@icloud.com>
2025-09-18 12:46:27 +0300

    Updated index.css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,76 +23,9 @@ export const Hero: React.FC<HeroProps> = ({ language, ...props }) => {
   }, []);
   
   return (
-    <div className="min-h-screen relative overflow-hidden bg-black dynamic-bg" {...props}>
-      {/* Cursor Following Gradient */}
-      <div className="cursor-gradient"></div>
-      
-      {/* Dynamic Background Layers */}
-      <div className="diagonal-gradient"></div>
-      <div className="wave-gradient"></div>
-      <div className="mesh-gradient"></div>
-      
-      {/* Floating Gradient Orbs */}
-      <div className="gradient-orb gradient-orb-1"></div>
-      <div className="gradient-orb gradient-orb-2"></div>
-      <div className="gradient-orb gradient-orb-3"></div>
-      
-      {/* Logo Background Elements */}
-      <div className="logo-watermark">Allync</div>
-      <div className="lettermark-a">A</div>
-      <div className="logo-glow"></div>
-      <div className="geometric-pattern"></div>
-      
-      {/* Logo Particles */}
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-
-      {/* Cursor Following Gradient */}
-      <div className="cursor-gradient"></div>
-      
-      {/* Dynamic Background Layers */}
-      <div className="diagonal-gradient"></div>
-      <div className="wave-gradient"></div>
-      <div className="mesh-gradient"></div>
-      
-      {/* Floating Gradient Orbs */}
-      <div className="gradient-orb gradient-orb-1"></div>
-      <div className="gradient-orb gradient-orb-2"></div>
-      <div className="gradient-orb gradient-orb-3"></div>
-      
-      {/* Logo Background Elements */}
-      <div className="logo-watermark">Allync</div>
-      <div className="lettermark-a">A</div>
-      <div className="logo-glow"></div>
-      <div className="geometric-pattern"></div>
-      
-      {/* Logo Particles */}
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-      <div className="logo-particle"></div>
-
-      {/* Particle background */}
-      <div className="particles">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className="particle"></div>
-        ))}
-      </div>
-
-      {/* Morphing shapes */}
-      <div className="absolute top-1/4 left-1/4 morph-shape"></div>
-      <div className="absolute bottom-1/3 right-1/3 morph-shape" style={{ animationDelay: '2s' }}></div>
-
-      {/* Animated background elements */}
-      <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-white/5 rounded-full blur-3xl breathe"></div>
-        <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-gray-500/10 rounded-full blur-3xl breathe delay-1000"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-72 h-72 bg-white/3 rounded-full blur-3xl breathe delay-500"></div>
-      </div>
+    <div className="min-h-screen relative bg-black" {...props}>
+      {/* Clean gradient background */}
+      <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-black to-gray-900"></div>
 
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-32 pb-16">
         <div className="text-center">

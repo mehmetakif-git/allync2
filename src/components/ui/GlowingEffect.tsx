73786072c7dc7d83e1b480f64b059b84d887f405
mehmetakif-git <mehmetakifaybey@icloud.com>
2025-10-05 15:25:35 +0300

    Updated GlowingEffect.tsx

--- src/components/ui/GlowingEffect.tsx
+++ src/components/ui/GlowingEffect.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export interface GlowingEffectProps {
   spread?: number;

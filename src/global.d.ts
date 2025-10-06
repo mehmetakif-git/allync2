909ec15c002084daec5809ed1c3b6448dce2dee8
mehmetakif-git <mehmetakifaybey@icloud.com>
2025-10-06 20:46:29 +0300

    Updated global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,28 +1,11 @@
+export { };
+
+declare module '*.glb';
+declare module '*.png';
+
 declare module 'meshline' {
-  import { BufferGeometry, Material } from 'three';
-
-  export class MeshLine extends BufferGeometry {
-    setGeometry(geometry: BufferGeometry | Float32Array): void;
-    setPoints(points: number[] | Float32Array): void;
-  }
-
-  export class MeshLineMaterial extends Material {
-    constructor(parameters?: any);
-    lineWidth: number;
-    color: THREE.Color;
-    opacity: number;
-    resolution: THREE.Vector2;
-    sizeAttenuation: number;
-    dashArray: number;
-    dashOffset: number;
-    dashRatio: number;
-    useMap: number;
-    useAlphaMap: number;
-    map: THREE.Texture | null;
-    alphaMap: THREE.Texture | null;
-    repeat: THREE.Vector2;
-    visibility: number;
-  }
+  export const MeshLineGeometry: any;
+  export const MeshLineMaterial: any;
 }
 
 declare global {
@@ -33,5 +16,3 @@ declare global {
     }
   }
 }
-
-export {};
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,4 @@
 /// <reference types="vite/client" />
+
 declare module '*.glb';
+declare module '*.png';
